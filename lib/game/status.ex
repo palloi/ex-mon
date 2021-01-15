117990a6b251846a0f937588ba3b3162df5b3eed
Palloi Hofmann <palloi.hofmann@gmail.com>
2021-01-15 20:16:29 -0300

    set test ex_mon

--- lib/game/status.ex
+++ lib/game/status.ex
@@ -1,38 +1,38 @@
 defmodule ExMon.Game.Status do
-	alias ExMon.Game
+  alias ExMon.Game
 
-	def print_round_message(%{status: :started} = info) do
-		IO.puts("\nThe game is started!\n")
-		IO.inspect(info)
-		IO.puts("---------")
-	end
+  def print_round_message(%{status: :started} = info) do
+    IO.puts("\nThe game is started!\n")
+    IO.inspect(info)
+    IO.puts("---------")
+  end
 
-	def print_round_message(%{status: :continue, turn: player} = info) do
-		IO.puts("\nIt's #{player} turn!\n")
-		IO.inspect(info)
-		IO.puts("---------")
-	end
+  def print_round_message(%{status: :continue, turn: player} = info) do
+    IO.puts("\nIt's #{player} turn!\n")
+    IO.inspect(info)
+    IO.puts("---------")
+  end
 
-	def print_round_message(%{status: :game_over}) do
-		IO.puts("\nThe game is over!\n")
-		IO.inspect(Game.info())
-		IO.puts("---------")
-	end
+  def print_round_message(%{status: :game_over}) do
+    IO.puts("\nThe game is over!\n")
+    IO.inspect(Game.info())
+    IO.puts("---------")
+  end
 
-	def print_wrong_move_message(move) do
-		IO.puts("\nInvalid move: #{move}\n")
-		IO.puts("---------")
-	end
+  def print_wrong_move_message(move) do
+    IO.puts("\nInvalid move: #{move}\n")
+    IO.puts("---------")
+  end
 
-	def print_move_message(:computer, :attack, damage) do
-		IO.puts("\nThe player attacked: dealing #{damage} damage.\n")
-	end
+  def print_move_message(:computer, :attack, damage) do
+    IO.puts("\nThe player attacked: dealing #{damage} damage.\n")
+  end
 
-	def print_move_message(:player, :attack, damage) do
-		IO.puts("\nThe computer attacked: dealing #{damage} damage.\n")
-	end
+  def print_move_message(:player, :attack, damage) do
+    IO.puts("\nThe computer attacked: dealing #{damage} damage.\n")
+  end
 
-		def print_heal_message(player, :heal, life) do
-		IO.puts("\nThe #{player} healled itselfe to #{life} life points.\n")
-	end
+    def print_heal_message(player, :heal, life) do
+    IO.puts("\nThe #{player} healled itselfe to #{life} life points.\n")
+  end
 end
---
+++ test/game_test.exs
@@ -0,0 +1,90 @@
+defmodule ExMon.GameTest do
+	use ExUnit.Case
+
+	alias ExMon.{Game, Player}
+
+	describe "start/2" do
+		test "start the game state" do
+			player = Player.build("Palloi", :punch, :kick, :heal)
+			computer = Player.build("Robo", :punch, :kick, :heal)
+
+			assert {:ok, _pid} = Game.start(computer, player)
+		end
+
+	end
+
+	describe "info/0" do
+		test "return the current game state" do
+			player = Player.build("Palloi", :punch, :kick, :heal)
+			computer = Player.build("Robo", :punch, :kick, :heal)
+
+			Game.start(computer, player)
+			expected_response = %{computer: %Player{life: 100, moves: %{move_avg: :kick, move_heal: :heal, move_rnd: :punch}, name: "Robo"}, player: %Player{life: 100, moves: %{move_avg: :kick, move_heal: :heal, move_rnd: :punch}, name: "Palloi"}, status: :started, turn: :player}
+
+			assert Game.info() == expected_response
+		end
+	end
+
+	describe "update/1" do
+		test "returns the game state update" do
+			player = Player.build("Palloi", :punch, :kick, :heal)
+			computer = Player.build("Robo", :punch, :kick, :heal)
+
+			Game.start(computer, player)
+			expected_response = %{computer: %Player{life: 100, moves: %{move_avg: :kick, move_heal: :heal, move_rnd: :punch}, name: "Robo"}, player: %Player{life: 100, moves: %{move_avg: :kick, move_heal: :heal, move_rnd: :punch}, name: "Palloi"}, status: :started, turn: :player}
+
+			assert Game.info() == expected_response
+
+			new_state = %{computer: %Player{life: 100, moves: %{move_avg: :kick, move_heal: :heal, move_rnd: :punch}, name: "Robo"}, player: %Player{life: 100, moves: %{move_avg: :kick, move_heal: :heal, move_rnd: :punch}, name: "Palloi"}, status: :started, turn: :player}
+			Game.update(new_state)
+			expected_response = %{new_state | turn: :computer, status: :continue}
+
+			assert Game.info() == expected_response
+		end
+	end
+
+	describe "player/0" do
+		test "return the player state info" do
+			player = Player.build("Palloi", :punch, :kick, :heal)
+			computer = Player.build("Robo", :punch, :kick, :heal)
+
+			Game.start(computer, player)
+
+			expected_player =  %Player{life: 100, moves: %{move_avg: :kick, move_heal: :heal, move_rnd: :punch}, name: "Palloi"}
+			assert Game.player() == expected_player
+		end
+	end
+
+	describe "turn/0" do
+		test "return the turn state info" do
+			player = Player.build("Palloi", :punch, :kick, :heal)
+			computer = Player.build("Robo", :punch, :kick, :heal)
+
+			Game.start(computer, player)
+
+			assert Game.turn() == :player
+		end
+	end
+
+	describe "fetch_player/0" do
+		test "return the player state info" do
+			player = Player.build("Palloi", :punch, :kick, :heal)
+			computer = Player.build("Robo", :punch, :kick, :heal)
+
+			Game.start(computer, player)
+			expected_player = %Player{life: 100, moves: %{move_avg: :kick, move_heal: :heal, move_rnd: :punch}, name: "Palloi"}
+
+			assert Game.fetch_player(:player) == expected_player
+		end
+
+		test "return the computer state info" do
+			player = Player.build("Palloi", :punch, :kick, :heal)
+			computer = Player.build("Robo", :punch, :kick, :heal)
+
+			Game.start(computer, player)
+			expected_computer =  %Player{life: 100, moves: %{move_avg: :kick, move_heal: :heal, move_rnd: :punch}, name: "Robo"}
+			
+			assert Game.fetch_player(:computer) == expected_computer
+		end
+	end
+end
