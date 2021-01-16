630f0f85dadd0d9be84ea207b7a65887fe020e51
Palloi Hofmann <palloi.hofmann@gmail.com>
2021-01-16 11:49:20 -0300

    test to ex_mon and game_test

--- test/ex_mon_test.exs
+++ test/ex_mon_test.exs
@@ -1,7 +1,64 @@
 defmodule ExMonTest do
   use ExUnit.Case
 
-  test "greets the world" do
-    assert true == true
+  import ExUnit.CaptureIO
+
+  alias ExMon.Player
+
+  describe "create_player/4" do
+    test "returns a player" do
+      expected_player = %Player{
+        life: 100,
+        moves: %{move_avg: :punch, move_heal: :heal, move_rnd: :kick},
+        name: "Palloi"
+      }
+
+      assert ExMon.create_player("Palloi", :kick, :punch, :heal) == expected_player
+    end
+  end
+
+  describe "start_game/1" do
+    test "when the game started, return a message" do
+      player = Player.build("Palloi", :punch, :kick, :heal)
+
+      messages = capture_io(fn ->
+        assert ExMon.start_game(player) == :ok
+      end)
+
+      assert messages =~ "The game is started"
+      assert messages =~ "status: :started"
+      assert messages =~ "turn: :player"
+    end
+  end
+
+  describe "make_move/1" do
+    setup do
+      player = Player.build("Palloi", :punch, :kick, :heal)
+
+      capture_io(fn ->
+        ExMon.start_game(player)
+      end)
+
+      :ok
+    end
+
+    test "when the move is valid, do the move and the computer makes a move" do
+      messages = capture_io(fn ->
+        ExMon.make_move(:kick)
+      end)
+
+      assert messages =~ "The player attacked"
+      assert messages =~ "It's computer turn!"
+      assert messages =~ "It's player turn!"
+      assert messages =~ "status: :continue"
+    end
+
+    test "when the move is invalid, returns an error message" do
+      messages = capture_io(fn ->
+        ExMon.make_move(:bosta)
+      end)
+
+      assert messages =~ "Invalid move: bosta"
+    end
   end
 end
--- test/game_test.exs
+++ test/game_test.exs
@@ -1,90 +1,90 @@
 defmodule ExMon.GameTest do
-	use ExUnit.Case
+  use ExUnit.Case
 
-	alias ExMon.{Game, Player}
+  alias ExMon.{Game, Player}
 
-	describe "start/2" do
-		test "start the game state" do
-			player = Player.build("Palloi", :punch, :kick, :heal)
-			computer = Player.build("Robo", :punch, :kick, :heal)
+  describe "start/2" do
+    test "start the game state" do
+      player = Player.build("Palloi", :punch, :kick, :heal)
+      computer = Player.build("Robo", :punch, :kick, :heal)
 
-			assert {:ok, _pid} = Game.start(computer, player)
-		end
+      assert {:ok, _pid} = Game.start(computer, player)
+    end
 
-	end
+  end
 
-	describe "info/0" do
-		test "return the current game state" do
-			player = Player.build("Palloi", :punch, :kick, :heal)
-			computer = Player.build("Robo", :punch, :kick, :heal)
+  describe "info/0" do
+    test "return the current game state" do
+      player = Player.build("Palloi", :punch, :kick, :heal)
+      computer = Player.build("Robo", :punch, :kick, :heal)
 
-			Game.start(computer, player)
-			expected_response = %{computer: %Player{life: 100, moves: %{move_avg: :kick, move_heal: :heal, move_rnd: :punch}, name: "Robo"}, player: %Player{life: 100, moves: %{move_avg: :kick, move_heal: :heal, move_rnd: :punch}, name: "Palloi"}, status: :started, turn: :player}
+      Game.start(computer, player)
+      expected_response = %{computer: %Player{life: 100, moves: %{move_avg: :kick, move_heal: :heal, move_rnd: :punch}, name: "Robo"}, player: %Player{life: 100, moves: %{move_avg: :kick, move_heal: :heal, move_rnd: :punch}, name: "Palloi"}, status: :started, turn: :player}
 
-			assert Game.info() == expected_response
-		end
-	end
+      assert Game.info() == expected_response
+    end
+  end
 
-	describe "update/1" do
-		test "returns the game state update" do
-			player = Player.build("Palloi", :punch, :kick, :heal)
-			computer = Player.build("Robo", :punch, :kick, :heal)
+  describe "update/1" do
+    test "returns the game state update" do
+      player = Player.build("Palloi", :punch, :kick, :heal)
+      computer = Player.build("Robo", :punch, :kick, :heal)
 
-			Game.start(computer, player)
-			expected_response = %{computer: %Player{life: 100, moves: %{move_avg: :kick, move_heal: :heal, move_rnd: :punch}, name: "Robo"}, player: %Player{life: 100, moves: %{move_avg: :kick, move_heal: :heal, move_rnd: :punch}, name: "Palloi"}, status: :started, turn: :player}
+      Game.start(computer, player)
+      expected_response = %{computer: %Player{life: 100, moves: %{move_avg: :kick, move_heal: :heal, move_rnd: :punch}, name: "Robo"}, player: %Player{life: 100, moves: %{move_avg: :kick, move_heal: :heal, move_rnd: :punch}, name: "Palloi"}, status: :started, turn: :player}
 
-			assert Game.info() == expected_response
+      assert Game.info() == expected_response
 
-			new_state = %{computer: %Player{life: 100, moves: %{move_avg: :kick, move_heal: :heal, move_rnd: :punch}, name: "Robo"}, player: %Player{life: 100, moves: %{move_avg: :kick, move_heal: :heal, move_rnd: :punch}, name: "Palloi"}, status: :started, turn: :player}
-			Game.update(new_state)
-			expected_response = %{new_state | turn: :computer, status: :continue}
+      new_state = %{computer: %Player{life: 100, moves: %{move_avg: :kick, move_heal: :heal, move_rnd: :punch}, name: "Robo"}, player: %Player{life: 100, moves: %{move_avg: :kick, move_heal: :heal, move_rnd: :punch}, name: "Palloi"}, status: :started, turn: :player}
+      Game.update(new_state)
+      expected_response = %{new_state | turn: :computer, status: :continue}
 
-			assert Game.info() == expected_response
-		end
-	end
+      assert Game.info() == expected_response
+    end
+  end
 
-	describe "player/0" do
-		test "return the player state info" do
-			player = Player.build("Palloi", :punch, :kick, :heal)
-			computer = Player.build("Robo", :punch, :kick, :heal)
+  describe "player/0" do
+    test "return the player state info" do
+      player = Player.build("Palloi", :punch, :kick, :heal)
+      computer = Player.build("Robo", :punch, :kick, :heal)
 
-			Game.start(computer, player)
+      Game.start(computer, player)
 
-			expected_player =  %Player{life: 100, moves: %{move_avg: :kick, move_heal: :heal, move_rnd: :punch}, name: "Palloi"}
-			assert Game.player() == expected_player
-		end
-	end
+      expected_player =  %Player{life: 100, moves: %{move_avg: :kick, move_heal: :heal, move_rnd: :punch}, name: "Palloi"}
+      assert Game.player() == expected_player
+    end
+  end
 
-	describe "turn/0" do
-		test "return the turn state info" do
-			player = Player.build("Palloi", :punch, :kick, :heal)
-			computer = Player.build("Robo", :punch, :kick, :heal)
+  describe "turn/0" do
+    test "return the turn state info" do
+      player = Player.build("Palloi", :punch, :kick, :heal)
+      computer = Player.build("Robo", :punch, :kick, :heal)
 
-			Game.start(computer, player)
+      Game.start(computer, player)
 
-			assert Game.turn() == :player
-		end
-	end
+      assert Game.turn() == :player
+    end
+  end
 
-	describe "fetch_player/0" do
-		test "return the player state info" do
-			player = Player.build("Palloi", :punch, :kick, :heal)
-			computer = Player.build("Robo", :punch, :kick, :heal)
+  describe "fetch_player/0" do
+    test "return the player state info" do
+      player = Player.build("Palloi", :punch, :kick, :heal)
+      computer = Player.build("Robo", :punch, :kick, :heal)
 
-			Game.start(computer, player)
-			expected_player = %Player{life: 100, moves: %{move_avg: :kick, move_heal: :heal, move_rnd: :punch}, name: "Palloi"}
+      Game.start(computer, player)
+      expected_player = %Player{life: 100, moves: %{move_avg: :kick, move_heal: :heal, move_rnd: :punch}, name: "Palloi"}
 
-			assert Game.fetch_player(:player) == expected_player
-		end
+      assert Game.fetch_player(:player) == expected_player
+    end
 
-		test "return the computer state info" do
-			player = Player.build("Palloi", :punch, :kick, :heal)
-			computer = Player.build("Robo", :punch, :kick, :heal)
+    test "return the computer state info" do
+      player = Player.build("Palloi", :punch, :kick, :heal)
+      computer = Player.build("Robo", :punch, :kick, :heal)
 
-			Game.start(computer, player)
-			expected_computer =  %Player{life: 100, moves: %{move_avg: :kick, move_heal: :heal, move_rnd: :punch}, name: "Robo"}
-			
-			assert Game.fetch_player(:computer) == expected_computer
-		end
-	end
+      Game.start(computer, player)
+      expected_computer =  %Player{life: 100, moves: %{move_avg: :kick, move_heal: :heal, move_rnd: :punch}, name: "Robo"}
+      
+      assert Game.fetch_player(:computer) == expected_computer
+    end
+  end
 end
